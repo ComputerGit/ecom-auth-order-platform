fa4606a72ff5af523e375277e843be116ab7294e
ComputerGit <pcmeanscomputer@gmail.com>
2026-02-09 11:52:06 +0530

    Update order integration test

--- src/test/java/com/at/t/eCommerce/boundedcontext/order/OrderFlowIntegrationTest.java
+++ src/test/java/com/at/t/eCommerce/boundedcontext/order/OrderFlowIntegrationTest.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.MediaType;
@@ -17,7 +16,6 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.at.t.eCommerce.TestJwtFactory;
-import com.at.t.eCommerce.config.TestSecurityConfig;
 
 import jakarta.transaction.Transactional;
 
